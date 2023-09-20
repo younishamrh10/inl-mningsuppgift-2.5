f2c3d6b2ce729d6eab703757554f4fa6a6a29be7
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-09-20 11:04:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace uppgift2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriva in en beräkning som innehållande två decimaltal och ett gångertecken");
+            string beräkning = Console.ReadLine();
+            int gångerindex = beräkning.IndexOf("*");
+            string decimal1text = beräkning[..gångerindex];
+            string decimal2text = beräkning[(gångerindex + 1)..];
+            float decimal1 = float.Parse(decimal1text);
+            float decimal2 = float.Parse(decimal2text);
+            float svar = decimal1 * decimal2;
+            Console.WriteLine("svare är" + svar);
+        }
+    }
+}
